916ffefca29b823734311bb4722c46844ab63011
Nayem Ahmed <144725995+nayem-ahmedz@users.noreply.github.com>
2024-01-31 00:48:10 +0600

    Create head.js

---
+++ comp/head.js
@@ -0,0 +1,18 @@
+//for header
+document.addEventListener('DOMContentLoaded', function () {
+  // Load Header
+  fetch('/comp/header.html')
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`Network response was not ok (${response.status} ${response.statusText})`);
+          }
+          return response.text();
+      })
+      .then(html => {
+          document.getElementById('header-container').innerHTML = html;
+      })
+      .catch(error => {
+          // Handle errors during the fetch
+          console.error('Error fetching header:', error.message);
+      });
+});
