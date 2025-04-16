a6f62d32c0fe6d6634a78cada01466b27b10626d
nayem-ahmedz <nayemahmed2n@gmail.com>
2025-04-16 17:27:11 +0600

    updated styles and added responsive nav bar in smaller screen (unfinished)

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 var pYear = document.getElementById('pYear');
 pYear.innerHTML = "2024";
+
+let nav = document.querySelector('nav');
+function toggleNav(a){
+    a.classList.toggle('change');
+    nav.classList.toggle('bringNav');
+    nav.style.overflow = 'visible';
+}
